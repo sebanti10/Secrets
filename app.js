31f764502136f99ace3aae7823d80a30965aa9b3
sebanti <sebanc99@gmail.com>
2020-06-29 01:10:54 +0530

    Level 2- Encryption

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const encrypt = require("mongoose-encryption")
 const app = express();
 
 app.use(express.static("public"));
@@ -14,11 +14,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema =
-{
+const userSchema = new mongoose.Schema
+({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
